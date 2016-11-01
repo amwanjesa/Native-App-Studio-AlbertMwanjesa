70b015fac2e118618558f720f4722c1f3abd065c
amwanjesa <a.mwanjesa@gmail.com>
2016-11-01 13:06:07 +0100

    Design done, buttons done

--- app/src/main/java/com/example/albert/albertpset1/MainActivity.java
+++ app/src/main/java/com/example/albert/albertpset1/MainActivity.java
@@ -5,16 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
 
-public abstract class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity {
 
-    private CheckBox arms, body, ears, brows, eyes, glass, hat, mouth, mustache, nose, shoes;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
     public void onCheckboxClicked(View view) {
@@ -30,6 +29,60 @@ public abstract class MainActivity extends AppCompatActivity implements View.OnC
                 // Remove the meat
                 break;
             case R.id.body:
+                if (checked)
+                // Cheese me
+                else
+                // I'm lactose intolerant
+                break;
+            case R.id.ears:
+                if (checked)
+                // Cheese me
+                else
+                // I'm lactose intolerant
+                break;
+            case R.id.brows:
+                if (checked)
+                // Cheese me
+                else
+                // I'm lactose intolerant
+                break;
+            case R.id.eyes:
+                if (checked)
+                // Cheese me
+                else
+                // I'm lactose intolerant
+                break;
+            case R.id.glass:
+                if (checked)
+                // Cheese me
+                else
+                // I'm lactose intolerant
+                break;
+            case R.id.hat:
+                if (checked)
+                // Cheese me
+                else
+                // I'm lactose intolerant
+                break;
+            case R.id.mouth:
+                if (checked)
+                // Cheese me
+                else
+                // I'm lactose intolerant
+                break;
+            case R.id.mustache:
+                if (checked)
+                // Cheese me
+                else
+                // I'm lactose intolerant
+                break;
+            case R.id.nose:
+                if (checked)
+                // Cheese me
+                else
+                // I'm lactose intolerant
+                break;
+            case R.id.shoes:
                 if (checked)
                 // Cheese me
                 else
