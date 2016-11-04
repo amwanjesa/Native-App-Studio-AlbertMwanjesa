b2f453a1f183abea142c70ec9e23f1aa98183494
amwanjesa <a.mwanjesa@gmail.com>
2016-11-04 12:32:57 +0100

    Design done, buttons done

--- app/src/main/java/com/example/albert/albertpset1/MainActivity.java
+++ app/src/main/java/com/example/albert/albertpset1/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.albert.albertpset1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void onCheckboxClicked(View view) {
@@ -21,73 +24,99 @@ public class MainActivity extends AppCompatActivity {
         boolean checked = ((CheckBox) view).isChecked();
 
         // Check which checkbox was clicked
+        String viewName = view.getResources().getResourceName(view.getId());
+        Log.d("MainActivity", "id:" + viewName);
         switch(view.getId()) {
             case R.id.arms:
-                if (checked)
-                // Put some meat on the sandwich
-                else
-                // Remove the meat
+                ImageView arms = (ImageView) findViewById(R.id.arms_image);
+                if (checked){
+                    arms.setVisibility(View.VISIBLE);
+                }else{
+                    arms.setVisibility(View.GONE);
+                }
                 break;
-            case R.id.body:
-                if (checked)
-                // Cheese me
-                else
-                // I'm lactose intolerant
-                break;
+
             case R.id.ears:
-                if (checked)
-                // Cheese me
-                else
-                // I'm lactose intolerant
+                ImageView ears = (ImageView) findViewById(R.id.ears_image);
+                if (checked){
+                    ears.setVisibility(View.VISIBLE);
+                }else{
+                    ears.setVisibility(View.GONE);
+                }
                 break;
+                
             case R.id.brows:
-                if (checked)
-                // Cheese me
-                else
-                // I'm lactose intolerant
+                ImageView brows = (ImageView) findViewById(R.id.brows_image);
+                if (checked){
+                    brows.setVisibility(View.VISIBLE);
+                }else{
+                    brows.setVisibility(View.GONE);
+                }
                 break;
+                
             case R.id.eyes:
-                if (checked)
-                // Cheese me
-                else
-                // I'm lactose intolerant
+                ImageView eye = (ImageView) findViewById(R.id.eye_image);
+                if (checked){
+                    eye.setVisibility(View.VISIBLE);
+                }else{
+                    eye.setVisibility(View.GONE);
+                }
                 break;
+                
             case R.id.glass:
-                if (checked)
-                // Cheese me
-                else
-                // I'm lactose intolerant
+                ImageView glass = (ImageView) findViewById(R.id.glass_image);
+                if (checked){
+                    glass.setVisibility(View.VISIBLE);
+                }else{
+                    glass.setVisibility(View.GONE);
+                }
                 break;
+                
             case R.id.hat:
-                if (checked)
-                // Cheese me
-                else
-                // I'm lactose intolerant
+                ImageView hat = (ImageView) findViewById(R.id.hat_image);
+                if (checked){
+                    hat.setVisibility(View.VISIBLE);
+                }else{
+                    hat.setVisibility(View.GONE);
+                }
                 break;
+                
             case R.id.mouth:
-                if (checked)
-                // Cheese me
-                else
-                // I'm lactose intolerant
+                ImageView mouth = (ImageView) findViewById(R.id.mouth_image);
+                if (checked){
+                    mouth.setVisibility(View.VISIBLE);
+                }else{
+                    mouth.setVisibility(View.GONE);
+                }
                 break;
+                
             case R.id.mustache:
-                if (checked)
-                // Cheese me
-                else
-                // I'm lactose intolerant
+                ImageView mustache = (ImageView) findViewById(R.id.mustache_image);
+                if (checked){
+                    mustache.setVisibility(View.VISIBLE);
+                }else{
+                    mustache.setVisibility(View.GONE);
+                }
                 break;
+                
             case R.id.nose:
-                if (checked)
-                // Cheese me
-                else
-                // I'm lactose intolerant
+                ImageView nose = (ImageView) findViewById(R.id.nose_image);
+                if (checked){
+                    nose.setVisibility(View.VISIBLE);
+                }else{
+                    nose.setVisibility(View.GONE);
+                }
                 break;
+                
             case R.id.shoes:
-                if (checked)
-                // Cheese me
-                else
-                // I'm lactose intolerant
+                ImageView shoes = (ImageView) findViewById(R.id.shoes_image);
+                if (checked){
+                    shoes.setVisibility(View.VISIBLE);
+                }else{
+                    shoes.setVisibility(View.GONE);
+                }
                 break;
+                
         }
     }
 }
